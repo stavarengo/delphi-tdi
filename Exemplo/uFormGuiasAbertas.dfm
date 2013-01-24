8ec11142149cc0039914e4901a9cfbf49fcb6b26
Stavarengo <faelsta@gmail.com>
2013-01-24 09:21:05 -0200

    Aprimorado visualização de abas abertas.

--- Exemplo/uFormGuiasAbertas.dfm
+++ Exemplo/uFormGuiasAbertas.dfm
@@ -1,9 +1,9 @@
 object FormGuiasAbertas: TFormGuiasAbertas
   Left = 375
   Top = 197
-  Width = 531
-  Height = 444
   Caption = 'Formul'#225'rio abertos'
+  ClientHeight = 406
+  ClientWidth = 515
   Color = clWhite
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,20 +11,25 @@ object FormGuiasAbertas: TFormGuiasAbertas
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  OnActivate = FormActivate
   OnClose = FormClose
-  OnShow = FormShow
   PixelsPerInch = 96
   TextHeight = 13
   object ScrollBox1: TScrollBox
     Left = 0
     Top = 0
-    Width = 523
-    Height = 410
+    Width = 515
+    Height = 406
     HorzScrollBar.Increment = 46
     HorzScrollBar.Tracking = True
     VertScrollBar.Increment = 36
     VertScrollBar.Tracking = True
     Align = alClient
     TabOrder = 0
+  end
+  object Timer1: TTimer
+    OnTimer = Timer1Timer
+    Left = 120
+    Top = 96
   end
 end
--- Exemplo/uFormGuiasAbertas.pas
+++ Exemplo/uFormGuiasAbertas.pas
@@ -9,8 +9,10 @@ uses
 type
   TFormGuiasAbertas = class(TForm)
     ScrollBox1: TScrollBox;
+    Timer1: TTimer;
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
-    procedure FormShow(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormActivate(Sender: TObject);
   private
     Visualizador: IVisualizador;
   public
@@ -26,14 +28,21 @@ uses DateUtils, Types, VisualizaImagensDasGuiasAbertas, uFormPrincipal;
 
 {$R *.dfm}
 
+procedure TFormGuiasAbertas.FormActivate(Sender: TObject);
+begin
+    Visualizador := TVisualizaImagensDasGuiasAbertas.Create(ScrollBox1, FTDI);
+  FTDI.VisualizarAbas(Visualizador, TFormGuiasAbertas);
+end;
+
 procedure TFormGuiasAbertas.FormClose(Sender: TObject;
   var Action: TCloseAction);
 begin
   Visualizador := nil;
 end;
 
-procedure TFormGuiasAbertas.FormShow(Sender: TObject);
+procedure TFormGuiasAbertas.Timer1Timer(Sender: TObject);
 begin
+  Timer1.Enabled := false;
   Visualizador := TVisualizaImagensDasGuiasAbertas.Create(ScrollBox1, FTDI);
   FTDI.VisualizarAbas(Visualizador, TFormGuiasAbertas);
 end;
